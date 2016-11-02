f0828290f62adae26a8f0cae7d9ae99d3128b12f
rouz majlessi <rmajlessi@gmail.com>
2016-11-02 14:25:38 -0700

    fix syntax errors for totalCost for loop

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -11,9 +11,9 @@ export default Ember.Service.extend({
   },
   totalCost: Ember.computed('items.[]', function() {
     var allCost = 0;
-    for (var i = 0, < this.get('items.length'); i ++) {
+    for (var i = 0; i < this.get('items.length'); i++) {
       allCost += this.get('items')[i].get('cost');
     }
     return allCost;
-  }
+  })
 });
