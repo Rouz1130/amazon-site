c6dd5c0742cd5d7ae3c8fae6a282a052c9effc3a
rouz majlessi <rmajlessi@gmail.com>
2016-11-02 11:57:46 -0700

    set attriubtes for model

--- app/models/item.js
+++ app/models/item.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  title: DS.attr('title'),
+  description: DS.attr('description'),
+  image: DS.attr('image'),
+  cost: DS.attr('cost')
 });
