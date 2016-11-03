e3e6b63fc90fb1a9932d7759c8f53b4bf59f54ee
rouz majlessi <rmajlessi@gmail.com>
2016-11-03 11:41:18 -0700

    add fucntion to for admin to remove item, also send action to delete and add delete button in template

--- app/components/remove-item.js
+++ app/components/remove-item.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     removeItem(item) {
-      if (confirm('Are you sure you want to delete item')) {
+      if(confirm("Are you sure you want to delete this from Nordstrom")) {
         this.sendAction('destroyItem', item);
       }
     }
--- tests/integration/components/remove-item-test.js
+++ tests/integration/components/remove-item-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('review-item', 'Integration | Component | review item', {
+moduleForComponent('remove-item', 'Integration | Component | remove item', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{review-item}}`);
+  this.render(hbs`{{remove-item}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#review-item}}
+    {{#remove-item}}
       template block text
-    {{/review-item}}
+    {{/remove-item}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
