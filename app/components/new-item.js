621690e7c2f17af7cb10d46974e74dcb46de92bb
rouz majlessi <rmajlessi@gmail.com>
2016-11-03 11:00:36 -0700

    add form logic to new tiem with a save function

---
+++ app/components/new-item.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewItem: false,
+  actions: {
+    itemFormShow() {
+      this.set('addNewItem', true);
+    },
+    saveItem() {
+      var params = {
+        title: this.get('title'),
+        image: this.get('image'),
+        description: this.get('description'),
+        cost: parseInt(this.get('cost'))
+      };
+      this.set('addNewItem', true);
+      this.sendAction('saveItem', params);
+    }
+  }
+});
