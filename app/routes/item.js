c3f518c7689a3ed5c5c65a54c96c194992e5b45f
rouz majlessi <rmajlessi@gmail.com>
2016-11-03 10:47:28 -0700

    add new review component file and popuulate it with a form to save reivew and showform, also make a review list component file

--- app/routes/item.js
+++ app/routes/item.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('item', params.item_id);
+  },
+  actions: {
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      console.log(params.item.get('reviews'));
+    }
   }
+
 });
