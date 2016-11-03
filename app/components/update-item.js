93cccfc61c542681b6fac05f464f3466a7bbac39
rouz majlessi <rmajlessi@gmail.com>
2016-11-03 11:52:31 -0700

    allow admin to update item, write update method

--- app/components/update-item.js
+++ app/components/update-item.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateItemForm: false,
+  actions: {
+    updateItemForm() {
+      this.set('updateItemForm', true);
+    },
+    updateItem(item) {
+      var params = {
+        title: this.get('title'),
+        image: this.get('image'),
+        description: this.get('description'),
+        cost: this.get('cost')
+      };
+      this.set('updateItemForm', false);
+      this.sendAction('updateItem', item, params);
+    }
+  }
 });
