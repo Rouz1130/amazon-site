01e595c401a9ca5c1f9bcdbdc757196081b5ce77
rouz majlessi <rmajlessi@gmail.com>
2016-11-03 10:30:42 -0700

    add action for new review and add save function and setters and getters

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewReview: false
+
+  actions: {
+    reviewFormShow () {
+      this.set('addNewReview', ture);
+    },
+
+    saveReview() {
+      var params = {
+        author: this.get('author'),
+        comment: this.get('comment'),
+        item: this.get('item')
+      };
+      this.set('addNewReview', false);
+      this.set('saveReview', params)
+    }
+  }
 });
